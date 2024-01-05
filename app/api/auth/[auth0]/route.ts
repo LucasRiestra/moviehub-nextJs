26efe416c16614f721cf70063e6dc78cee48da88
Lucas Riestra <Lucas.riestra94@gmail.com>
2024-01-05 11:39:53 +0100

    deploy42

--- app/api/auth/[auth0]/route.ts
+++ app/api/auth/[auth0]/route.ts
@@ -4,7 +4,7 @@ import { handleAuth, handleLogin } from '@auth0/nextjs-auth0';
 export const GET = handleAuth({
     login: handleLogin({
         authorizationParams: {
-            audience: "http://localhost:4001/"
+            audience: "http://localhost:4001"
         },
         returnTo: "/home"
     })
