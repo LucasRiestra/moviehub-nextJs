df86faec45f1ebbcc65061fe419e9ecc836d01fc
Lucas Riestra <Lucas.riestra94@gmail.com>
2024-01-05 11:58:09 +0100

    deploy46

--- app/api/auth/[auth0]/route.ts
+++ app/api/auth/[auth0]/route.ts
@@ -1,13 +1,14 @@
+// app/api/auth/[auth0]/route.js
 import { handleAuth, handleLogin, handleLogout } from '@auth0/nextjs-auth0';
 
-export default handleAuth({
-  login: handleLogin({
-    authorizationParams: {
-      audience: "http://localhost:4001/"
-    },
-    returnTo: "/home"
-  }),
-  logout: handleLogout({
-    returnTo: '/home',
-  }),
-});
+export const GET = handleAuth({
+    login: handleLogin({
+        authorizationParams: {
+          audience: "http://localhost:4001/"
+        },
+        returnTo: "/home"
+      }),
+      logout: handleLogout({
+        returnTo: '/home',
+      }),
+    });
