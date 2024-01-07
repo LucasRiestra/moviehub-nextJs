b72e099e3a5fa0da691f274343cf615e198ccb39
Lucas Riestra <Lucas.riestra94@gmail.com>
2024-01-07 18:28:47 +0100

    deploy61

--- components/Header/header.tsx
+++ components/Header/header.tsx
@@ -37,7 +37,7 @@ const Header = () => {
     setIsReloading(true);
     setTimeout(() => {
       window.location.href = '/home';
-    }, 1000);
+    }, 100);
   };
 
   const handleToggleChange = () => {
